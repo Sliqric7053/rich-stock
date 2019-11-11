9ab0d197f2a5cb48a8769edcacb1b9b90977322a
Richard Sithole <richardsit@gmail.com>
2019-11-11 08:07:08 +0200

    Add photo rating component

--- rich-stock/libs/rich-stock/ui/src/lib/rich-stock-ui.module.ts
+++ rich-stock/libs/rich-stock/ui/src/lib/rich-stock-ui.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+import { material } from './ui-material';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PhotoRatingComponent } from './components/photo-rating/photo-rating.component';
+
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ...material
+  ],
+  declarations: [AppLayoutComponent, PhotoRatingComponent],
+  exports: [AppLayoutComponent, PhotoRatingComponent, ...material]
 })
 export class RichStockUiModule {}
